1465f89804cafbab11c32cd8bfcec93209dbd13f
Miguel da Silva Pataro <pataro.miguel@gmail.com>
2023-09-04 18:22:53 -0300

    first comit

---
+++ Atividade3/ED1I4.Atividade3/ED1I4.Atividade3/OpcoesEnum.cs
@@ -0,0 +1,23 @@
+﻿// ATIVIDADE REALIZADA POR:
+
+//     - MIGUEL DA SILVA PATARO
+//     - JOÃO VITOR PEDRAL
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED1I4.Atividade3
+{
+    public enum OpcoesEnum
+    {
+        Sair = 0,
+        Adicionar = 1,
+        Pesquisar = 2,
+        Alterar = 3,
+        Remover = 4,
+        Listar = 5
+    }
+}
